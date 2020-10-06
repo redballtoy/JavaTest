393325b34086fd13d73cd4c31dc99d88b7634ae8
Dmitry Sorokin <redballtoy@gmail.com>
2020-10-06 18:23:01 +0300

    Geekbgains lesson 07 анимация падения капли

--- Study/src/aaa_GeekbrainsStudy/IntroductionInJavaPlatform/gpaphicGame/GameWindow.java
+++ Study/src/aaa_GeekbrainsStudy/IntroductionInJavaPlatform/gpaphicGame/GameWindow.java
@@ -17,7 +17,13 @@ public class GameWindow extends JFrame {
 
     //координаты капли, левого верхнего угла
     private static float dropLeft = 200;
-    private static float dropTop = 200;
+    private static float dropTop = -100; //-100 что бы вылетала из-за верхней границы экрана
+
+    //подсчет времени прошедшем между перерисовками (кадрами)
+    private static long last_frame_time;
+
+    //скорость капли
+    private static float velocityDrop = 200;
 
 
 
@@ -41,6 +47,8 @@ public class GameWindow extends JFrame {
         gameWindow.setResizable(false);
         //добавляем панель в окно
         GameField gameField = new GameField();
+        //присваиваем начальное значение переменной с временем фрейма
+        last_frame_time = System.nanoTime();
         gameWindow.add(gameField);
         //сделать видимым
         gameWindow.setVisible(true);
@@ -49,6 +57,14 @@ public class GameWindow extends JFrame {
 
     //добавление возможности рисовать в окне
     private static void onRepaid(Graphics g) {
+
+        //получим текущее время
+        long current_time = System.nanoTime();
+        //рассчитаем разницу во времени и переведем из наносекунд в секунды
+        float timeDelta = (current_time - last_frame_time) * 0.000000001f;
+        //присвоим начальному значению время текущего кадра
+        last_frame_time = current_time;
+
         //рисование в панели g фигур
         /*g.setColor(Color.BLUE);
         g.fillOval(100,100,300,200);
@@ -56,8 +72,12 @@ public class GameWindow extends JFrame {
 
         //рисование загруженных картинок
         g.drawImage(background, 0, 0, null);
-        g.drawImage(drop, (int)dropLeft, (int)dropTop, null);
         //g.drawImage(game_over, 280, 120, null);
+
+        //анимирование капли
+        dropTop = dropTop + velocityDrop * timeDelta;
+        g.drawImage(drop, (int)dropLeft, (int)dropTop, null);
+
 
     }
 
@@ -69,6 +89,7 @@ public class GameWindow extends JFrame {
         protected void paintComponent(Graphics g) {
             //для отрисовки необходимо получить доступ к методу paintComponent в классе JPanel
             super.paintComponent(g);
+            //метод игрового уровня
             onRepaid(g);
             //перерисовывать панель
             repaint();
