99e140436ab9c2c453d4a1e7537ef7238483364e
Dmitry Sorokin <redballtoy@gmail.com>
2020-10-19 11:27:27 +0300

    BinaryTree поиск элемента и нахожденрие min и max

--- Study/src/aaa_GeekbrainsStudy/Algorithms/BinaryTree.java
+++ Study/src/aaa_GeekbrainsStudy/Algorithms/BinaryTree.java
@@ -1,29 +1,29 @@
 package aaa_GeekbrainsStudy.Algorithms;
 
 /*
-* Дерево - это:
-*   - структура данных имеющая древовидную структуру в виде набора связанных узлов
-*   - является связным графом
-*   - не содержит циклы
-*
-* Преимуществом является то что операции удаления вставки и поиска осуществляются
-*   быстро
-*
-* Составные части:
-*   - корневой узел - самый верхний узел дерева
-*   - корень - одна из вершин по желанию наблюдателя
-*   - лист (терминальный узел) - узел не имеющий дочерних элементов
-*   - внутренний узел - любой узел дерева имеющий потомков и соответственно не
-*       являющийся листом
-*   - дерево считается ориентированным если в корень не заходит ни одно ребро
-*/
+ * Дерево - это:
+ *   - структура данных имеющая древовидную структуру в виде набора связанных узлов
+ *   - является связным графом
+ *   - не содержит циклы
+ *
+ * Преимуществом является то что операции удаления вставки и поиска осуществляются
+ *   быстро
+ *
+ * Составные части:
+ *   - корневой узел - самый верхний узел дерева
+ *   - корень - одна из вершин по желанию наблюдателя
+ *   - лист (терминальный узел) - узел не имеющий дочерних элементов
+ *   - внутренний узел - любой узел дерева имеющий потомков и соответственно не
+ *       являющийся листом
+ *   - дерево считается ориентированным если в корень не заходит ни одно ребро
+ */
 
 /*
-* Двоичное (бинарное дерево) это дерево в котором корень имеет не более двух потомков
-*   - самый верхний узел назвается корневым а его дели правыми и левыми наследниками
-*   - не является упорядоченным ориентированным деревом
-*   - имеет строгую структуру
-* */
+ * Двоичное (бинарное дерево) это дерево в котором корень имеет не более двух потомков
+ *   - самый верхний узел назвается корневым а его дели правыми и левыми наследниками
+ *   - не является упорядоченным ориентированным деревом
+ *   - имеет строгую структуру
+ * */
 
 
 public class BinaryTree {
@@ -35,7 +35,7 @@ public class BinaryTree {
 }
 
 //описание данных которые будут храниться в деревьях
-class Person{
+class Person {
     public String name;
     public int id;//обязательный атрибут на основе которого будет производиться сортировка (уникальный)
     public int age;
@@ -48,31 +48,65 @@ class Person{
 }
 
 //это сам узел в связанном списке
-class Node{
+class Node {
     public Person person;
     public Node leftChild;//фактически ребра которые являются ссылками на предыдущий и
     public Node rightChild;//последующий элемент
 
-    public void display(){
-        System.out.println("Имя: "+person.name+"\tВозраст: "+person.age);
+    public void display() {
+        System.out.println("Имя: " + person.name + "\tВозраст: " + person.age);
     }
 
 }
 
 //класс создания дерева
-class Tree{
+class Tree {
     private Node root;//корневой узел, наша точка отчета
 
-    //вставка нового Person
+    //вставка нового Person (сначала ищет узел для вставки, затем если id<idузла вставит как левый потомок иначе как
+    // правый)
     public void insert(Person person) {
+        Node node = new Node();
+        node.person = person;
+        if (root == null) {
+            root = node;
+        } else {
+            Node current = root;
+            Node parent;
+            while (true) {
+                parent = current;
+                if (person.id < current.person.id) {
+                    current = current.leftChild;
+                    if (current == null) {
+                        parent.leftChild = node;
+                        return;
+                    }
+                } else {
+                    current = current.rightChild;
+                    if (current == null) {
+                        parent.rightChild = node;
+                        return;
+                    }
+                }
+            }
+        }
 
     }
 
     //поиск узла по ключу
     public Node find(int key) {
-        Node node = new Node();
-
-        return node;
+        Node current = root;
+        while (current.person.id != key) {
+            if (key < current.person.id) {
+                current = current.leftChild;
+            } else {
+                current = current.rightChild;
+                if (current == null) {
+                    return null;//дошли до листа, что означает что не нашли требуемый узел
+                }
+            }
+        }
+        return current;
     }
 
     //удаление узла по id
@@ -82,8 +116,39 @@ class Tree{
     }
 
     //вывод содержимого всего дерева
-    public void display_Tree(){
+    public void display_Tree() {
 
+    }
+
+    //реализация симметричного обхода
+    private void inOrder(Node rootNode) {
+        if (rootNode != null) {
+            inOrder(rootNode.leftChild);
+            rootNode.display();
+            inOrder(rootNode.rightChild);
+        }
+    }
+
+    Node min() {
+        Node current = null;
+        Node last = null;
+        current = root;
+        while (current != null) {
+            last = current;
+            current = current.leftChild;
+        }
+        return last;
+    }
+
+    Node max() {
+        Node current = null;
+        Node last = null;
+        current = root;
+        while (current != null) {
+            last = current;
+            current = current.rightChild;
+        }
+        return last;
     }
 
 
