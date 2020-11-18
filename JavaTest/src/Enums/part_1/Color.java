ccb481074ad6a5cc462707a706234b76cdf0e989
Dmitry Sorokin <redballtoy@gmail.com>
2020-11-18 14:09:11 +0300

    lesson 01_02 fin

--- JavaTest/src/Enums/part_1/Color.java
+++ JavaTest/src/Enums/part_1/Color.java
@@ -1,9 +1,17 @@
 package Enums.part_1;
 
 /*Enumeric - это класс который наследуется от Object
+ *   - это класс с заранее заготовленным списком и опредением того что с этим списком может
+ *      происходить
  *   - указанные в Enum перечисления являются объектами (анонимными классами)
  *   - перечислениям можно указывать поля
  *   - у каждого перечисления есть конструктор по умолчанию  BLACK()
+ *   - enum нельзя ни от чего наследовать
+ *   - у enum конструктора по умолчанию установлен private что бы нельзя было изменить объеты в нем
+ *      public Color(String russianColor, String englishColor) { - будет ошибка
+ *   - enum может реализовывать интерфейс поскольку для enum это будет просто еще один метод
+ *      (желательно не нагружать enum логикой, только то что нужно для заполнения и вывода
+ *          либо один только action в SQL_Command )
  *
  * */
 
@@ -18,6 +26,7 @@ public enum Color {
     private String russianColor, englishColor;
 
     //при создании конструктора переопределяется конструктор по умолчению
+    //по умолчанию он private
     Color(String russianColor, String englishColor) {
         this.russianColor = russianColor;
         this.englishColor = englishColor;
