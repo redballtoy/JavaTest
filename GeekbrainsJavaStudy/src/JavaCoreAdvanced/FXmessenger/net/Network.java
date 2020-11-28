6a9eae9d76bd3f686927a49d9a30dfc2b1fafe52
Dmitry Sorokin <redballtoy@gmail.com>
2020-11-28 14:58:10 +0300

    FX Messenger start Networking

---
+++ GeekbrainsJavaStudy/src/JavaCoreAdvanced/FXmessenger/net/Network.java
@@ -0,0 +1,7 @@
+package JavaCoreAdvanced.FXmessenger.net;
+
+//класс который полностью отвечает за взаимодействие между сервером и клиентом (view ) через контроллер
+public class Network {
+    public static final int
+
+}
